fac7d1b90ce44f83884a658e912b70e9eff15bfa
Tim Macfarlane <timmacfarlane@gmail.com>
2011-10-14 08:17:50 +0100

    don't need these!

--- src/jungle/if.js
+++
@@ -1,1 +0,0 @@
-var condition,doStuff,doOtherStuff;condition=1;doStuff=function(){return console.log([2,3]);};doOtherStuff=function(){return console.log([3,4]);};if(condition){doStuff();}else{doOtherStuff();}
