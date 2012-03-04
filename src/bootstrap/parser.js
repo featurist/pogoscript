ee9e2da767c94733e81560ab032506294f6dce70
Tim Macfarlane <timmacfarlane@gmail.com>
2012-03-04 10:12:34 +0000

    fixed require

--- src/bootstrap/parser.js
+++ src/bootstrap/parser.js
@@ -6,7 +6,7 @@
     ms = require("../lib/memorystream");
     createParserContext = require("./parserContext").createParserContext;
     createDynamicLexer = require("./dynamicLexer").createDynamicLexer;
-    grammar = require("./grammar.pogo").grammar;
+    grammar = require("./grammar").grammar;
     parser = new jisonParser(grammar);
     jisonLexer = parser.lexer;
     createParser = function() {
